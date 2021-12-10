44e2666ba3f231745ef70ce7155ed3e85dd25fff
Jeferson Mesquita <je_2742@hotmail.com>
2021-12-10 00:47:12 -0300

    Utilizando Icones
    Chapter II Styled Components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import {
+	Container,
+	Header,
+	UserWrapper,
+	UserInfo,
+	Photo,
+	User,
+	UserGreeting,
+	UserName,
+	Icon,
+} from './styles';
+
+export function Dashboard() {
+	return (
+		<Container>
+			<Header>
+				<UserWrapper>
+					<UserInfo>
+						<Photo
+							source={{
+								uri: 'https://avatars.githubusercontent.com/u/20876017?v=4',
+							}}
+						/>
+						<User>
+							<UserGreeting>Olá,</UserGreeting>
+							<UserName>Jeferson</UserName>
+						</User>
+					</UserInfo>
+
+					<Icon name="power" />
+				</UserWrapper>
+			</Header>
+		</Container>
+	);
+}
