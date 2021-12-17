d043d7886888e878998d47f69325447e750f42db
Jeferson Mesquita <je_2742@hotmail.com>
2021-12-16 21:59:58 -0300

    Utilizando a Tab Navigation
    Navegação

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { NavigationContainer } from '@react-navigation/native';
 import AppLoading from 'expo-app-loading';
 
 import {
@@ -9,8 +10,9 @@ import {
 	Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
 
+import { AppRoutes } from './src/routes/app.routes';
+
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -25,7 +27,9 @@ export default function App() {
 
 	return (
 		<ThemeProvider theme={theme}>
-			<Register />
+			<NavigationContainer>
+				<AppRoutes />
+			</NavigationContainer>
 		</ThemeProvider>
 	);
 }
